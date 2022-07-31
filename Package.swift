3bca497d073c7df757f714d2ecb60bc177695ac3
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-07-31 13:04:30 -0400

    init commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "my-app",
+    dependencies: [
+        .package(url: "../my-library", branch: "master"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "my-app",
+            dependencies: [
+             .product(name: "my-library", package: "my-library"),
+
+            ]),
+        .testTarget(
+            name: "my-appTests",
+            dependencies: ["my-app"]),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# my-app
+
+A description of this package.
