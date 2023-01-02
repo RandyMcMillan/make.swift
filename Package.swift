ffd62f1fffa7ca231e0e3ad9799b99d56c085ebd
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-02 18:27:19 -0500

    Package.swift:

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "my-app",
     dependencies: [
-        .package(url: "my-library", branch: "master"),
+        .package(url: "https://github.com/RandyMcMillan/my-library.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
