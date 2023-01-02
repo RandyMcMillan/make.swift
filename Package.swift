7ecf1133a0ea94f7de490d8851abfcc86b0ea043
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-02 18:48:04 -0500

    make: default building

--- Package.swift
+++ Package.swift
@@ -1,24 +1,46 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+//REF: https://developer.apple.com/documentation/xcode/creating-a-standalone-swift-package-with-xcode
 
 import PackageDescription
 
 let package = Package(
     name: "my-app",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "my-app",
+            targets: ["my-app"])////, "SomeRemoteBinaryPackage", "SomeLocalBinaryPackage"])
+    ],
     dependencies: [
-        .package(url: "https://github.com/RandyMcMillan/my-library.git", .branch("master")),
+        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "my-app",
-            dependencies: [
-             .product(name: "my-library", package: "my-library"),
-
-            ]),
+            exclude: ["instructions.md"],
+            resources: [
+                .process("text.txt"),
+                .process("example.png"),
+                .copy("settings.plist")
+            ]
+        ),
+        //.binaryTarget(
+        //    name: "SomeRemoteBinaryPackage",
+        //    url: "https://url/to/some/remote/binary/package.zip",
+        //    checksum: "The checksum of the XCFramework inside the ZIP archive."
+        //),
+        //.binaryTarget(
+        //    name: "SomeLocalBinaryPackage",
+        //    path: "path/to/some.xcframework"
+        //)
         .testTarget(
-            name: "my-appTests",
-            dependencies: ["my-app"]),
+             name: "my-appTests",
+             dependencies: ["my-app"]),
     ]
 )
