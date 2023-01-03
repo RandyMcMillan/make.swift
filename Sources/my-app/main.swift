6c4e24c102304df01ab4fe1e76d34e3b97ddb287
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-02 20:41:34 -0500

    Sources/my-app/main.swift: swift version detection

--- Sources/my-app/main.swift
+++ Sources/my-app/main.swift
@@ -1,23 +1,39 @@
 #if os(Linux)
 //import myLibrary
 
+#if swift(>=5.3.2)
+    print("Running Swift 5.3.2 or later")
+#else
+    print("Running Swift 5.3.2 or earlier")
+#endif
+
 // Code specific to Linux
 print("Hello, Linux world!")
 
 #elseif os(macOS)
 //import myLibrary
 
+#if swift(>=5.3.2)
+    print("Running Swift 5.3.2 or later")
+#else
+    print("Running Swift 5.3.2 or earlier")
+#endif
+
 // Code specific to macOS
 print("Hello, macOS world!")
-
 #endif
 
 #if canImport(UIKit)
 //import myLibrary
 
+#if swift(>=5.3.2)
+    print("Running Swift 5.3.2 or later")
+#else
+    print("Running Swift 5.3.2 or earlier")
+#endif
+
 // Code specific to platforms where UIKit is available
 print("Hello, UIKit world!")
-
 #endif
 
 print("Hello, main.swift!")
