18e5490ea6e5bf3ebbd61e1a21296f91341be8f7
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-02 20:19:58 -0500

    Sources/my-app/main.swift: detect Linux/macOS/UIKit

--- Sources/my-app/main.swift
+++ Sources/my-app/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+#if os(Linux)
+//import myLibrary
+
+// Code specific to Linux
+print("Hello, Linux world!")
+
+#elseif os(macOS)
+//import myLibrary
+
+// Code specific to macOS
+print("Hello, macOS world!")
+
+#endif
+
+#if canImport(UIKit)
+//import myLibrary
+
+// Code specific to platforms where UIKit is available
+print("Hello, UIKit world!")
+
+#endif
+
+print("Hello, main.swift!")
